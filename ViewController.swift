8f57ca3b036ab8c532b07d13817e6b0fa470a773
Seth-alkins <91442486+Seth-alkins@users.noreply.github.com>
2021-09-30 13:58:42 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714 - F2021 - Assignment 1
+//  Author: Seth Alkins
+//  Student ID: 301232944
+//  Date: 9/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
